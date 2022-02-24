47311f5e4b3fd406372d3d7425da17157507a5cc
egarinad <zavadskij76@gmail.com>
2022-02-24 15:26:45 +0300

    a

---
+++ js/Reviews.js
@@ -0,0 +1,7 @@
+const img = document.querySelector('.person-img');
+const name = document.querySelector('.review__name');
+const job = document.querySelector('.review__name');
+const info = document.querySelector('.review__info');
+
+const nextButton = document.querySelector('.buttons__next-button');
+const prevButton = document.querySelector('.buttons__prev-button');
